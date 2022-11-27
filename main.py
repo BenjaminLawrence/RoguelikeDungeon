c29e91dc33a5c9866dc4e5948052c97916ea1c69
Benjamin Lawrence <83344040+BenjaminLawrence@users.noreply.github.com>
2022-11-28 00:10:59 +0800

    feat: Added a new tileset for generating graphics.

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ def main() -> None:
     screen_width = 80
     screen_height = 50
 
+    tileset = tcod.tileset.load_tilesheet(
+        "dejavu10x10_gs_tc.png", 32, 8, tcod.tilset.CHARMAP_TCOD
+    )
+
     with tcod.context.new_terminal(
         screen_width,
         screen_height,
+        tilset=tileset,
         title="Roguelike Dungeon",
         vsync=True,
     ) as context:
