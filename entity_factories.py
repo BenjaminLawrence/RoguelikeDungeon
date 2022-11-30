012642b8ad807c43a493187926f3dfdf05e59fbd
Benjamin Lawrence <83344040+BenjaminLawrence@users.noreply.github.com>
2022-11-30 12:14:20 +0800

    Fix typo
    Changed "block_movement" to the proper "blocks_movement" for the Troll
    entity.

--- entity_factories.py
+++ entity_factories.py
@@ -5,4 +5,4 @@ player = Entity(char="@", color=(255, 255, 255), name="Player", blocks_movement=
 
 # Creation of Enemies
 orc = Entity(char="o", color=(63, 127, 63), name="Orc", blocks_movement=True)
-troll = Entity(char="T", color=(0, 127, 0), name="Troll", block_movement=True)
+troll = Entity(char="T", color=(0, 127, 0), name="Troll", blocks_movement=True)
