632fe7cc5d8868c3810f0b8d0b0564fe378b696a
Benjamin Lawrence <83344040+BenjaminLawrence@users.noreply.github.com>
2022-12-05 13:20:40 +0800

    Add drop item action
    Added a drop item action to handle dropping items from an actor's
    inventory.

--- actions.py
+++ actions.py
@@ -83,6 +83,11 @@ class EscapeAction(Action):
         raise SystemExit()
 
 
+class DropItem(ItemAction):
+    def perform(self) -> None:
+        self.entity.inventory.drop(self.item)
+
+
 class WaitAction(Action):
     def perform(self) -> None:
         pass
