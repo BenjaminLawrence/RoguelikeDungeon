4b19d4aac43da65c00c94bf2a79d820bf7e6f10d
Benjamin Lawrence <83344040+BenjaminLawrence@users.noreply.github.com>
2022-12-05 12:02:20 +0800

    Add inventories to actors
    Added an inventory for each actor to store items.

--- entity.py
+++ entity.py
@@ -9,6 +9,7 @@ if TYPE_CHECKING:
     from components.ai import BaseAI
     from components.consumable import Consumable
     from components.fighter import Fighter
+    from components.inventory import Inventory
     from game_map import GameMap
 
 T = TypeVar("T", bound="Entity")
@@ -83,7 +84,8 @@ class Actor(Entity):
         color: Tuple[int, int, int] = (255, 255, 255),
         name: str = "<Unnamed>",
         ai_cls: Type[BaseAI],
-        fighter: Fighter
+        fighter: Fighter,
+        inventory: Inventory,
     ):
         super().__init__(
             x=x,
@@ -92,13 +94,16 @@ class Actor(Entity):
             color=color,
             name=name,
             blocks_movement=True,
-            render_order=RenderOrder.ACTOR
+            render_order=RenderOrder.ACTOR,
         )
 
         self.ai: Optional[BaseAI] = ai_cls(self)
 
         self.fighter = fighter
         self.fighter.parent = self
+
+        self.inventory = inventory
+        self.inventory.parent = self
 
     @property
     def is_alive(self) -> bool:
