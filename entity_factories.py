fbe9c125472b4c2c653738463ca2af2dbd8ed099
Benjamin Lawrence <83344040+BenjaminLawrence@users.noreply.github.com>
2022-12-06 21:08:22 +0800

    Add fireball scrolls
    Added a factory to handle the creation of fireball scrolls.

--- entity_factories.py
+++ entity_factories.py
@@ -40,6 +40,12 @@ confusion_scroll = Item(
     name="Confusion Scroll",
     consumable=consumable.ConfusionConsumable(number_of_turns=10),
 )
+fireball_scroll = Item(
+    char="~",
+    color=(255, 0, 0),
+    name="Fireball Scroll",
+    consumable=consumable.FireballDAmageConsumable(damage=12, radius=3),
+)
 health_potion = Item(
     char="!",
     color=(127, 0, 255),
