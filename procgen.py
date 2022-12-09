69d64498613c241473d5218ad5c91921f4ee1c92
Benjamin Lawrence <83344040+BenjaminLawrence@users.noreply.github.com>
2022-12-09 10:54:39 +0800

    Fix missing symbol
    Added a "=" that was missing in procgen.py.

--- procgen.py
+++ procgen.py
@@ -111,7 +111,7 @@ def generate_dungeon(
 
     rooms: List[RectangularRoom] = []
 
-    center_of_last_room (0, 0)
+    center_of_last_room = (0, 0)
 
     for r in range(max_rooms):
         room_width = random.randint(room_min_size, room_max_size)
