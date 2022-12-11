2e3fdb23869b26e11d294265f35fdf94cd88552e
Benjamin Lawrence <83344040+BenjaminLawrence@users.noreply.github.com>
2022-12-12 01:11:23 +0800

    Add equipment to Actors
    Added equipment to the factories for Actors.

--- entity_factories.py
+++ entity_factories.py
@@ -1,5 +1,6 @@
 from components.ai import HostileEnemy
 from components import consumable, equippable
+from components.equipment import Equipment
 from components.fighter import Fighter
 from components.inventory import Inventory
 from components.level import Level
@@ -12,6 +13,7 @@ player = Actor(
     color=(255, 255, 255),
     name="Player",
     ai_cls=HostileEnemy,
+    equipment=Equipment(),
     fighter=Fighter(hp=30, defence=2, power=5),
     inventory=Inventory(capacity=26),
     level=Level(level_up_base=200),
@@ -23,6 +25,7 @@ orc = Actor(
     color=(63, 127, 63),
     name="Orc",
     ai_cls=HostileEnemy,
+    equipment=Equipment(),
     fighter=Fighter(hp=10, defence=0, power=3),
     inventory=Inventory(capacity=0),
     level=Level(xp_given=35),
@@ -32,6 +35,7 @@ troll = Actor(
     color=(0, 127, 0),
     name="Troll",
     ai_cls=HostileEnemy,
+    equipment=Equipment(),
     fighter=Fighter(hp=16, defence=1, power=4),
     inventory=Inventory(capacity=0),
     level=Level(xp_given=100),
